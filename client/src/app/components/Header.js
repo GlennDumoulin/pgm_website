121f0c27bf85242fa65b47b1d3f8987e71fd88b2
pgmgent-glendumo <glendumo@student.arteveldehs.be>
2020-05-26 17:41:27 +0200

    Added header

--- client/src/app/components/Header.js
+++ client/src/app/components/Header.js
@@ -6,25 +6,24 @@ class Header {
   async render () {
     return `
       <header class="header">
-        <div class="container nav">
-          <div class="row">
-            <div class="nav_logo">
-              <a href="${routes.HOME}" data-navigo>
-                <img src="${pgmLogo}" alt="Logo PGM">
-              </a>
-            </div>
-            <div class="nav_pages">
-              <a href="${routes.OPLEIDING}" class="nav_pages--item" data-navigo>Opleiding</a>
-              <a href="${routes.PGMTEAM}" class="nav_pages--item" data-navigo>PGM-Team</a>
-              <a href="${routes.PORTFOLIO}" class="nav_pages--item" data-navigo>Portfolio</a>
-              <a href="${routes.NIEUWS}" class="nav_pages--item" data-navigo>Nieuws</a>
-              <a href="${routes.WERKPLEKLEREN}" class="nav_pages--item" data-navigo>Werkplekleren</a>
-              <a href="${routes.CONTACT}" class="nav_pages--item" data-navigo>Contact</a>
-            </div>
-            <div class="btn-hamburger">HAMB</div>
+        <nav class="nav row align-items-center justify-content-between">
+          <div class="nav_logo">
+            <a href="${routes.HOME}" data-navigo>
+              <img src="${pgmLogo}" alt="Logo PGM">
+            </a>
           </div>
-        </div>        
+          <ul class="nav_list d-flex justify-content-between">
+            <li class="nav_item"><a href="${routes.OPLEIDING}" data-navigo>Opleiding</a></li>
+            <li class="nav_item"><a href="${routes.PGMTEAM}" data-navigo>PGM-Team</a></li>
+            <li class="nav_item"><a href="${routes.PORTFOLIO}" data-navigo>Portfolio</a></li>
+            <li class="nav_item"><a href="${routes.NIEUWS}" data-navigo>Nieuws</a></li>
+            <li class="nav_item"><a href="${routes.WERKPLEKLEREN}" data-navigo>Werkplekleren</a></li>
+            <li class="nav_item"><a href="${routes.CONTACT}" data-navigo>Contact</a></li>
+          </ul>
+          <div class="nav_btn btn-hamburger">HAMB</div>
+        </nav>
       </header>
+      <div class="stroke"></div>
     `;
   }
 
@@ -38,12 +37,12 @@ class Header {
   }
 
   updateActiveLink (route) {
-    const prevActiveMenuItemElement = document.querySelector(`.nav__item > a[class*="active"]`);
+    const prevActiveMenuItemElement = document.querySelector(`.nav_item > a[class*="active"]`);
     if (prevActiveMenuItemElement) {
       prevActiveMenuItemElement.classList.remove('active');
     }
     const link = route.replace('#!', '');
-    const menuItemElement = document.querySelector(`.nav__item > a[href*="${link}"]`);
+    const menuItemElement = document.querySelector(`.nav_item > a[href*="${link}"]`);
     if (menuItemElement) {
       menuItemElement.classList.add('active');
     }
