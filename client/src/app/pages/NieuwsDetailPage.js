966838b063e528bdd87916620877fb5343f10ee8
pgmgent-glendumo <glendumo@student.arteveldehs.be>
2020-06-04 16:23:17 +0200

    Added styles for nieuws detail

--- client/src/app/pages/NieuwsDetailPage.js
+++ client/src/app/pages/NieuwsDetailPage.js
@@ -1,13 +1,85 @@
+import { BAAS } from '../services';
+
+import { routes } from '../router';
+
+import { PostsList } from '../components';
+
 class NieuwsDetailPage {
+  constructor () {
+    this.compPostsList = new PostsList(2);
+  }
+
+  async getPost () {
+    const searchLink = window.location.hash;
+    const searchId = searchLink.substring(searchLink.lastIndexOf('/') + 1);
+    const post = await BAAS.getPost(searchId);
+    return `
+      <div class="post">
+        <div class="row post-basic">
+          <div class="col-10 col-lg-11">
+            <h1 class="post-basic__item">${post.title}</h1>
+            <p>Gemaakt op ${this.getReadableDate(post.createdAt)}</p>
+          </div>
+          <div class="btn-back col-2 col-md-1">
+            <a href="#!${routes.NIEUWS}"><i class="fas fa-times"></i></a>
+          </div>
+        </div>
+        <div class="post-about">
+          <div class="card post-about__main">
+            <img src="${post.thumbnail}" alt="Thumbnail for post" class="post-about__main-image">
+            <div class="post-about__main-body">
+              ${post.body}
+            </div>
+          </div>
+          <div class="post-about__authors">
+            <h3>Geschreven door:</h3>
+            <ul>
+              ${this.getAuthors(post.authors)}
+            </ul>
+          </div>
+        </div>
+        <div class="post-tags">
+          <h2><i class="fas fa-tags no-borders"></i> Tags</h2>
+          <div class="d-flex">
+            ${this.getTags(post.tags)}
+          </div>
+        </div>
+        <div class="post-related">
+          <h2>Bekijk ook</h2>
+          ${await this.compPostsList.render()}
+        </div>
+      </div>
+    `;
+  }
+
+  getReadableDate (date) {
+    return new Date(date).toLocaleDateString();
+  }
+
+  getAuthors (array) {
+    return array.map(maker => `
+      <li>${maker}</li>
+    `).join('');
+  }
+
+  getTags (array) {
+    return array.map(tag => `
+      <p>${tag}</p>
+    `).join('');
+  }
+
   async render () {
     return `
       <div class="page page--nieuws_detail container">
-        <h1>Nieuws detail</h1>
+        ${await this.getPost()}
       </div>
     `;
   }
 
   async afterRender () {
+    // afterRender all components on the page
+    this.compPostsList.afterRender();
+
     // Connect the listeners
     return this;
   }
